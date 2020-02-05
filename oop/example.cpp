aa0c5c077b8cec9e983753ceb3c863dfe428a388
DropD <r.haeuselmann@gmx.ch>
2020-02-05 13:22:53 +0100

    add example running code

--- oop/example.cpp
+++ oop/example.cpp
@@ -1,9 +1,12 @@
+#include <iostream>
+
+
 struct Point
 {
-	flost _x;
+	float _x;
 	float _y;
 	Point(float x, float y): _x(x), _y(y) {}
-	Point(Point p): _x(p._x), _y(p._y) {}
+	Point(const Point& p): _x(p._x), _y(p._y) {}
 	void operator+=(const Point& p)
 	{	
 		_x += p._x;
@@ -17,6 +20,7 @@ struct Point
 
 
 class Shape {
+    public:
     Shape() : center_point(100, 100) {
         this->ref_count += 1;
     }
@@ -26,8 +30,67 @@ class Shape {
 
     private:
     static int ref_count;
+
+    protected:
     Point center_point;
 
     public:
-    virtual void draw() {};
+    virtual void draw() = 0;
+    virtual void move(const Point& vector) = 0;
 };
+
+
+class Rectangle : public Shape{
+    public:
+    Rectangle(const Point& top_left, const Point& bottom_right) : Shape(), top_left(top_left), bottom_right(bottom_right) {
+        float center_x = (top_left._x + bottom_right._x) / 2;
+        float center_y = (top_left._y + bottom_right._y) / 2;
+        this->center_point = Point(center_x, center_y);
+    }
+
+    private:
+    Point top_left;
+    Point bottom_right;
+
+    public:
+    virtual void draw() {
+        std::cout << "<Rectangle( (" << this->top_left._x << ", " << this->top_left._y << "), (" << this->bottom_right._x << ", " << this->bottom_right._y << ") )>" << std::endl;
+    }
+    virtual void move(const Point& vector) {
+        this->center_point += vector;
+        this->top_left += vector;
+        this->bottom_right += vector;
+    }
+
+};
+
+/*
+class Circle : public Shape{
+    //...
+};
+
+class Path : public Shape{
+    //...
+};
+*/
+
+
+namespace utils {
+
+void complex_transform(Shape& anything) {
+    anything.move(Point(5, 5));
+    anything.move(Point(0, 10));
+}
+
+void other_transforms(Shape& anything) {
+    //...
+}
+
+}
+
+int main() {
+    Point top_left(10, 10), bottom_right(100, 100);
+    Rectangle r = {top_left, bottom_right};
+    utils::complex_transform(r);
+    r.draw();
+}
