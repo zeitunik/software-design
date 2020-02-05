b3bc4015d8e7fdb4cec62f53de34c24aec3ef273
DropD <r.haeuselmann@gmx.ch>
2020-02-05 12:43:32 +0100

    add more code to Shape

--- oop/example.cpp
+++ oop/example.cpp
@@ -8,7 +8,17 @@ struct Point
 
 
 class Shape {
+    Shape() : center_point(100, 100) {
+        this->ref_count += 1;
+    }
+    Shape(float center_x, float center_y) : center_point(center_x, center_y) {
+        this->ref_count += 1;
+    }
+
     private:
     static int ref_count;
     Point center_point;
+
+    public:
+    virtual void draw() {};
 };
