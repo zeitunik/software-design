1e01b462ab4a00a0b76b2e912b21ed8fe2b7586c
DropD <r.haeuselmann@gmx.ch>
2020-02-12 10:53:19 +0100

    get to compile using cmake

---
+++ oop/tests.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//~ class Rectangle {
+    //~ public:
+    //~ void draw() {
+        //~ std::cout << "<Rectangle(" << ")>" << std::endl;
+    //~ }
+//~ };
+
+int main() {
+    //~ Rectangle r = {};
+    //~ r.draw();
+    std::cout << "Hello World!" << std::endl;
+}
